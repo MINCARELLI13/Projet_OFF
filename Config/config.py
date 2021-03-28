247f59fd46f2b27bcf4ae0541c7cf1e8370204d2
E Mincarelli <sancho.poncho@laposte.net>
2021-03-28 14:17:35 +0100

    capitalizes constants

--- Config/config.py
+++ Config/config.py
@@ -1,33 +1,31 @@
 # coding: utf-8
 
 """ Categories proposed to users """
-# categories = ['Snacks salés', 'Gâteaux', 'Sodas', 'Desserts glacés', 'Plats préparés']
-# categories = {'1': 'Snacks salés', '2': 'Gâteaux','3': 'Sodas','4': 'Desserts glacés','5': 'Plats préparés'}
-categories = {1: 'Snacks salés', 2: 'Gâteaux',3: 'Sodas',4: 'Desserts glacés',5: 'Plats préparés'}
+CATEGORIES = {1: 'Snacks salés', 2: 'Gâteaux',3: 'Sodas',4: 'Desserts glacés',5: 'Plats préparés'}
 
-# number of products to load per category
-products_nb = 10
+""" number of products to load per category """
+PRODUCTS_NB = 10
 
 """ user of MySQL """
-user = 'root'
+USER = 'root'
 
 """ password of connexion to SQL """
-password = '123ab456'
+PASSWORD = '123ab456'
 
 """ host of connexion to SQL """
-host = 'localhost'
+HOST = 'localhost'
  
 """ database used """
-database = 'BDD_OFF'
+DATABASE = 'BDD_OFF'
 
 """ begin of URL of API """
-url = 'https://fr.openfoodfacts.org/cgi/search.pl'
+URL = 'https://fr.openfoodfacts.org/cgi/search.pl'
 
 """ fields of the products to load """
-fields_of_products = ('url', 'product_name_fr',
+FIELDS_OF_PRODUCTS = ('url', 'product_name_fr',
                     'brands', 'nutrition_grade_fr',
                     'ingredients_text_fr', 'stores')
 
-name_of_product_fields = ('nom', 'marque', 'url', 'nutriscore', 'ingrédients', 'magasins')
+NAME_OF_PRODUCT_FIELDS = ('nom', 'marque', 'url', 'nutriscore', 'ingrédients', 'magasins')
 
-nb_of_substituts = 5
+NB_OF_SUBSTITUTS = 10
