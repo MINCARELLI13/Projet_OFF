f28b16daa48e2b57983504ed665325a61345a280
E Mincarelli <sancho.poncho@laposte.net>
2021-03-30 20:13:03 +0100

    cleaning with pylint

--- API/requests_api.py
+++ API/requests_api.py
@@ -1,27 +1,28 @@
+""" This module is used to search products on OpenFoodFats """
 # coding: utf-8
 import requests
 
 
-class Request_Api:
+class RequestApi:
     """ contains all the requests made on OpenFoodFacts's API """
 
-    def get_products_of_category(self, catg_name, fields_of_products, products_nb, url):
+    def get_products_of_category(self, catg_name, fields_products_api, products_nb, url):
         """
         For each category of products makes a request to OpenFoodFacts API
         In reception : all parameters to make request to API's OFF
-        On return    : list of dictionaries where each dictionary 
-                        contains all information on one product 
+        On return    : list of dictionaries where each dictionary
+                        contains all information on one product
         """
-        parameters_request_API = {
+        parameters_request_api = {
             'action': 'process', 'tagtype_0': 'categories',
             'tag_contains_0': 'contains', 'tag_0': catg_name,
-            'fields': ','.join(fields_of_products),
+            'fields': ','.join(fields_products_api),
             'page_size': products_nb, 'json': 'true'}
 
-        req = requests.get(url, params=parameters_request_API)
+        req = requests.get(url, params=parameters_request_api)
         # selects only useful informations on products
         return req.json().get('products')
 
 
-if __name__=='__main__':
+if __name__ == '__main__':
     pass
