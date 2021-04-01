68978c95c824b7c8ee2e2363bedbe82fbbd8e04b
E Mincarelli <sancho.poncho@laposte.net>
2021-04-01 14:00:39 +0100

    Rewrite Projet 5 with ORM method

---
+++ Model_ORM/category.py
@@ -0,0 +1,14 @@
+# coding: utf-8
+from ORM.model import Model
+
+
+class Category(Model):
+
+    def __init__(self):
+        Model.__init__(self)
+        self.table = "Category"
+        self.columns_read = ['id', 'name']
+        self.columns_update = ['name']
+        self.columns_create =  "id INT AUTO_INCREMENT,\
+                                name VARCHAR(100) NOT NULL UNIQUE,\
+                                PRIMARY KEY (id)"
