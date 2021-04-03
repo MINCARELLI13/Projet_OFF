7420f88b98f3a229f1a565f60c9aae4bfb8301a5
E Mincarelli <sancho.poncho@laposte.net>
2021-04-03 16:11:56 +0100

    renaming of modules used by this script

---
+++ main.py
@@ -0,0 +1,14 @@
+""" File to launches the main controller file """
+# coding: utf-8
+from Controller.controller_main import Control
+
+
+class Main:
+
+    def __init__(self):
+        self.control = Control()
+        self.control.main_menu()
+
+
+if __name__=='__main__':
+    main = Main()
