83c7837901306395c21999195dcef31a64aae6f6
E Mincarelli <sancho.poncho@laposte.net>
2021-04-03 12:37:04 +0100

    renaming of Model_ORM to Model

---
+++ Model/substitute.py
@@ -0,0 +1,26 @@
+# coding: utf-8
+from Model_ORM.model import Model
+
+
+class Substitute(Model):
+
+    def __init__(self):
+        Model.__init__(self)
+        self.table = "Substitutes"
+        self.columns_read = ['original_id', 'substitut_id']
+        self.columns_update = ['original_id', 'substitut_id']
+        self.columns_create =  "original_id INT NOT NULL, \
+                                substitut_id INT NOT NULL, \
+                                PRIMARY KEY (original_id, substitut_id), \
+                                CONSTRAINT fk_Substitutes_original_id \
+                                FOREIGN KEY (original_id) \
+                                REFERENCES BDD_OFF.Product (id) \
+                                ON DELETE NO ACTION \
+                                ON UPDATE NO ACTION, \
+                                CONSTRAINT fk_Substitutes_substitut_id \
+                                FOREIGN KEY (substitut_id) \
+                                REFERENCES BDD_OFF.Product (id) \
+                                ON DELETE NO ACTION \
+                                ON UPDATE NO ACTION"
+        self.columns_recorded = ['Original.name', 'Original.brand',
+                                 'Substitut.name', 'Substitut.brand']
