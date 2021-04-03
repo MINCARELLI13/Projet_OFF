1f3bb00434ac6f45e8d3c83cb0c752992bcac74f
E Mincarelli <sancho.poncho@laposte.net>
2021-04-03 21:27:00 +0100

    Modifications after using flake8

--- Model/substitute.py
+++ Model/substitute.py
@@ -11,19 +11,18 @@ class Substitute(RequestSql):
         self.table = "Substitutes"
         self.columns_read = ['original_id', 'substitut_id']
         self.columns_update = ['original_id', 'substitut_id']
-        self.columns_create =  "original_id INT NOT NULL, \
-                                substitut_id INT NOT NULL, \
-                                PRIMARY KEY (original_id, substitut_id), \
-                                CONSTRAINT fk_Substitutes_original_id \
-                                FOREIGN KEY (original_id) \
-                                REFERENCES BDD_OFF.Product (id) \
-                                ON DELETE NO ACTION \
-                                ON UPDATE NO ACTION, \
-                                CONSTRAINT fk_Substitutes_substitut_id \
-                                FOREIGN KEY (substitut_id) \
-                                REFERENCES BDD_OFF.Product (id) \
-                                ON DELETE NO ACTION \
+        self.columns_create = "original_id INT NOT NULL,\
+                                substitut_id INT NOT NULL,\
+                                PRIMARY KEY (original_id, substitut_id),\
+                                CONSTRAINT fk_Substitutes_original_id\
+                                FOREIGN KEY (original_id)\
+                                REFERENCES BDD_OFF.Product (id)\
+                                ON DELETE NO ACTION\
+                                ON UPDATE NO ACTION,\
+                                CONSTRAINT fk_Substitutes_substitut_id\
+                                FOREIGN KEY (substitut_id)\
+                                REFERENCES BDD_OFF.Product (id)\
+                                ON DELETE NO ACTION\
                                 ON UPDATE NO ACTION"
         self.columns_recorded = ['Original.name', 'Original.brand',
                                  'Substitut.name', 'Substitut.brand']
-                                 
